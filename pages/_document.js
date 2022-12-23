9d4fc22988765ded2ae28dd7bec16aa0f37b00bc
shriniket <shriniketkulkarni42@gmail.com>
2022-12-23 21:45:32 +0530

    first commit

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className="font-poppins">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
